174ecaa695cadd4b3b6b43101867f90da112d088
Asan Azimkulov <azimkulov40@gmail.com>
2022-10-05 08:02:38 +0600

    заканчивает финальную часть верстки

---
+++ OneDrive/Рабочий стол/кибербуллинг/app.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.variants', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// Сглаживание
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
